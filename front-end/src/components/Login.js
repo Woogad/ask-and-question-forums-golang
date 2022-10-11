b6934eb1b725063a369e5d1fe29c2b05e4234e16
Teerasak Jumwanta <vgthailand@hotmail.com>
2022-10-11 23:41:06 +0700

    update auth page

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,6 +1,27 @@
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import axios from "axios";
 function Login() {
+    const apiUrl = "http://localhost:8080/login"
+    const navigate = useNavigate()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submit = e => {
+        e.preventDefault()
+        axios.post(`${apiUrl}`, {
+
+            email: email,
+            password: password
+        }, { withCredentials: true }).then(res => {
+            alert("login successfully")
+            navigate("/")
+            window.location.reload();
+        }).catch(res => {
+            alert("fail to login")
+        })
+    }
+
     return (
         <div className='flex justify-center'>
             <div className='p-6 max-w-sm bg-white rounded-lg border border-amber-500 shadow-md w-80 h-fit mt-36 '>
@@ -9,12 +30,14 @@ function Login() {
                 </div>
 
                 <div>
-                    <form>
+                    <form onSubmit={submit}>
                         <div class="mt-6 ">
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">อีเมล</label>
-                            <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="email" required />
+                            <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                                onChange={(e) => setEmail(e.target.value)} placeholder="email" required />
                             <label for="Password" class="block mb-2 text-sm font-medium text-gray-900 mt-2 ">รหัสผ่าน</label>
-                            <input type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="password" required />
+                            <input type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                                onChange={(e) => setPassword(e.target.value)} placeholder="password" required />
 
                             <div className="flex justify-end mt-2">
                                 <Link to='/Register'>
@@ -24,9 +47,7 @@ function Login() {
 
                         </div>
                         <div className='flex justify-center'>
-                            <button class="bg-transparent hover:bg-sky-400 text-amber-500 font-semibold hover:text-white py-2 px-4 border border-amber-500 hover:border-transparent rounded">
-                                ยืนยัน
-                            </button>
+                            <input type="submit" value={"ยืนยัน"} class="bg-transparent hover:bg-sky-400 text-amber-500 font-semibold hover:text-white py-2 px-4 border border-amber-500 hover:border-transparent rounded" />
                         </div>
                     </form>
                 </div>
--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -1,4 +1,28 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 function Register() {
+
+    const apiUrl = "http://localhost:8080/register"
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const submit = (e) => {
+        e.preventDefault()
+        axios.post(`${apiUrl}`, {
+            name: name,
+            email: email,
+            password: password
+        }).then(res => {
+            alert("create user successfully")
+            navigate('/login')
+        }).catch(res => {
+            alert("fail to create user")
+        })
+    }
+
     return (
         <div className='flex justify-center'>
             <div className='p-6 max-w-sm bg-white rounded-lg border border-amber-500 shadow-md dark:border-gray-700  w-80 h-fit mt-36 '>
@@ -7,20 +31,21 @@ function Register() {
                 </div>
 
                 <div>
-                    <form className="my-4">
+                    <form className="my-4" onSubmit={submit}>
                         <div class="mb-6 ">
                             <label for="name" class="block mb-2 text-sm font-medium text-gray-900">ชื่อผู้ใช้</label>
-                            <input type="name" id="name" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name" required />
+                            <input type="text" id="name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                                onChange={e => setName(e.target.value)} placeholder="name" required />
                             <label for="Email" class="block mb-2 text-sm font-medium text-gray-900 ">อีเมล</label>
-                            <input type="Email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="email" required />
+                            <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                                onChange={e => setEmail(e.target.value)} placeholder="email" required />
                             <label for="Password" class="block mb-2 text-sm font-medium text-gray-900 0">รหัสผ่าน</label>
-                            <input type="Password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="password" required />
+                            <input type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                                onChange={e => setPassword(e.target.value)} placeholder="password" required />
                         </div>
 
                         <div className='flex justify-center'>
-                            <button class="bg-transparent hover:bg-sky-400 text-amber-500 font-semibold hover:text-white py-2 px-4 border border-amber-500 hover:border-transparent rounded">
-                                ยืนยัน
-                            </button>
+                            <input type="submit" class="bg-transparent hover:bg-sky-400 text-amber-500 font-semibold hover:text-white py-2 px-4 border border-amber-500 hover:border-transparent rounded" value={"ยืนยัน"} />
                         </div>
                     </form>
 
@@ -30,5 +55,5 @@ function Register() {
 
         </div>
     );
-} 
+}
 export default Register;
