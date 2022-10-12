519f576f38b50b0b9621e57eaa9f4c645071fbc7
Teerasak Jumwanta <vgthailand@hotmail.com>
2022-10-12 21:01:15 +0700

    add middleware to create-katoo and getKatoobyID

--- back-end/routes/katooRotes.go
+++ back-end/routes/katooRotes.go
@@ -2,12 +2,14 @@ package routes
 
 import (
 	"Woogad/ask-and-question-forums-golang/controller"
+	"Woogad/ask-and-question-forums-golang/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func KatooRotes(router *gin.Engine) {
 	router.GET("/katoo", controller.GetKatoo)
-	router.POST("/katoo", controller.CreateKatoo)
+	router.POST("/katoo", middleware.RequireAuth, controller.CreateKatoo)
 	router.DELETE("/katoo/:id", controller.DeleteKatooByID)
+	router.GET("/katoo/:katooID", controller.GetKatooByID)
 }
