23636e39bba36f02385e16af6c90b04be6be26f3
Teerasak Jumwanta <vgthailand@hotmail.com>
2022-10-12 21:00:27 +0700

    add (username,userID) to katoo models

--- back-end/models/katooMoel.go
+++ back-end/models/katooMoel.go
@@ -4,8 +4,9 @@ import "gorm.io/gorm"
 
 type Katoo struct {
 	gorm.Model
-	Title string `json:"title"`
-	Body  string `json:"body"`
-	Vote  int64  `json:"vote"`
-	// User  User
+	Title    string `json:"title"`
+	Body     string `json:"body"`
+	UserName string `json:"username"`
+	Vote     uint   `json:"vote"`
+	UserID   uint   `json:"userid"`
 }
