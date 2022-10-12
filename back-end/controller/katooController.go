9ed5b38506d30221c259dd0aa9f4116a0f49ebb0
Teerasak Jumwanta <vgthailand@hotmail.com>
2022-10-12 20:58:39 +0700

    update create read user

--- back-end/controller/katooController.go
+++ back-end/controller/katooController.go
@@ -20,8 +20,26 @@ func GetKatoo(c *gin.Context) {
 }
 
 func CreateKatoo(c *gin.Context) {
-	new_katoo := models.Katoo{}
-	c.BindJSON(&new_katoo)
+	var body struct {
+		Title string `json:"title"`
+		Body  string `json:"body"`
+	}
+	if c.BindJSON(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "fail to read body",
+		})
+		return
+	}
+	user, _ := c.Get("user")
+	userStruct := user.(models.User)
+
+	new_katoo := models.Katoo{
+		Title:    body.Title,
+		Body:     body.Body,
+		UserName: userStruct.Name,
+		UserID:   userStruct.ID,
+	}
+
 	result := config.DB.Create(&new_katoo)
 	if result.Error != nil {
 		c.IndentedJSON(http.StatusInternalServerError,
@@ -40,4 +58,15 @@ func DeleteKatooByID(c *gin.Context) {
 		return
 	}
 	c.IndentedJSON(http.StatusNoContent, &katoo)
+}
+
+func GetKatooByID(c *gin.Context) {
+	id := c.Param("katooID")
+	katoo := models.Katoo{}
+	if result := config.DB.Where("id = ?", id).Find(&katoo); result.Error != nil {
+		c.IndentedJSON(http.StatusInternalServerError,
+			gin.H{"Error": result.Error.Error()})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, &katoo)
 }
