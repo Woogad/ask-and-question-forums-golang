f904fa2196fdcf3a4567c35e9e03212b5d55a43d
Passakorn <ohm.mm196@gmail.com>
2022-10-14 01:24:33 +0700

    decorate detailpage

--- front-end/src/components/PostKatooData.js
+++ front-end/src/components/PostKatooData.js
@@ -3,15 +3,15 @@ import React from 'react'
 function PostKatooData(props) {
     const { ID, title, body } = props
     return (
-        <>
-            <div className='flex justify-start mt-5 border-solid border-2 border-sky-500'>
+        <div className='px-96'>
+            <div className='flex justify-start mt-5 border-solid border-2 border-amber-500 hover:bg-amber-200 p-2 rounded-xl'>
                 <div>
                     {/* <p>ID:{ID}</p> */}
                     <p className=''>{title}</p>
                     <p className='opacity-50'>{(body.length > 15) ? (body.slice(0, 15) + "...") : (body)}</p>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
--- front-end/src/components/PostKatooDetail.js
+++ front-end/src/components/PostKatooDetail.js
@@ -19,12 +19,33 @@ const PostKatooDetail = () => {
             })
     }, [])
     return (
-
         <div>
-            <p>katoo ID: {katoo.ID}</p>
-            <p>katoo title: {katoo.title}</p>
-            <p>katoo body: {katoo.body}</p>
-            <p>katoo username: {katoo.username}</p>
+            <div className='px-3'>
+
+                <div class="mt-20 bg-amber-600 p-10 rounded-2xl shadow-lg">
+                    <div class="text-white">
+                        <p class="">ไอดี:{katoo.ID} </p>
+                        <p class="text-5xl flex justify-center">หัวข้อเรื่อง:{katoo.title} </p>
+                    </div>
+                </div>
+
+                <div class="mt-5 bg-amber-600 p-10 rounded-2xl">
+                    <div class="text-white">
+                        <p class='text-2xl font-thin'>{katoo.body} </p>
+                        <p class="mt-20 text-lg">ผู้สร้าง:{katoo.username} </p>
+                    </div>
+                </div>
+
+                <div class="mt-5 bg-amber-600 p-5 rounded-2xl mb-5">
+                    <form action="" class="w-full p-5">
+                        <label class="">
+                            <span class="text-white">คอมเม้น</span>
+                            <textarea class="block w-full mt-1 rounded-2xl p-3" rows="5"></textarea>
+                        </label>
+                        <button class="px-3 py-2 text-sm text-blue-100 bg-blue-600 rounded mt-6">ส่งคอมเม้น</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
